a651d527b574d0de1d808992bb5a54b70ec2668f
Evan Ovadia <verdagon@gmail.com>
2019-12-08 15:50:27 -0800

    Attempted to use dotnet-webassembly to run wasm.

--- Assets/IncendianFalls/Scripts/RootPresenter.cs
+++ Assets/IncendianFalls/Scripts/RootPresenter.cs
@@ -5,6 +5,9 @@ using Atharia.Model;
 using UnityEngine;
 using UnityEngine.UI;
 using Domino;
+using WebAssembly; // Acquire from https://www.nuget.org/packages/WebAssembly
+using WebAssembly.Instructions;
+using WebAssembly.Runtime;
 
 namespace IncendianFalls {
   public class TimerEntry {
@@ -26,6 +29,12 @@ namespace IncendianFalls {
       }
       return Math.Sign(x.id - y.id);
     }
+  }
+
+  // We need this later to call the code we're generating.
+  public abstract class Sample {
+    // Sometimes you can use C# dynamic instead of building an abstract class like this.
+    public abstract int Demo(int value);
   }
 
   public class RootPresenter : MonoBehaviour, ITimer {
@@ -53,6 +62,10 @@ namespace IncendianFalls {
     public SoundPlayer soundPlayer;
 
     public void Start() {
+      Debug.LogError("starting!");
+      DoWasmThing();
+      Debug.LogError("done!");
+
       startTime = System.DateTime.UtcNow;
       timers = new SortedDictionary<TimerEntry, object>(new TimerEntryComparer());
       resumeStaller = new ExecutionStaller(this);
@@ -86,6 +99,80 @@ namespace IncendianFalls {
       playerController.Start();
 
       cameraController = new FollowingCameraController(cameraObject, game);
+    }
+
+    public void DoWasmThing() {
+      // Module can be used to create, read, modify, and write WebAssembly files.
+      var module = new Module(); // In this case, we're creating a new one.
+
+      // Types are function signatures: the list of parameters and returns.
+      module.Types.Add(new WebAssemblyType // The first added type gets index 0.
+      {
+        Parameters = new[]
+          {
+                WebAssemblyValueType.Int32, // This sample takes a single Int32 as input.
+                // Complex types can be passed by sending them in pieces.
+            },
+        Returns = new[]
+          {
+                // Multiple returns are supported by the binary format.
+                // Standard currently allows a count of 0 or 1, though.
+                WebAssemblyValueType.Int32,
+            },
+      });
+      // Types can be re-used for multiple functions to reduce WASM size.
+
+      // The function list associates a function index to a type index.
+      module.Functions.Add(new Function // The first added function gets index 0.
+      {
+        Type = 0, // The index for the "type" value added above.
+      });
+
+      // Code must be passed in the exact same order as the Functions above.
+      module.Codes.Add(new FunctionBody {
+        Code = new Instruction[]
+          {
+                new LocalGet(0), // The parameters are the first locals, in order.
+                // We defined the first parameter as Int32, so now an Int32 is at the top of the stack.
+                new Int32CountOneBits(), // Returns the count of binary bits set to 1.
+                // It takes the Int32 from the top of the stack, and pushes the return value.
+                // So, in the end, there is still a single Int32 on the stack.
+                new End(), // All functions must end with "End".
+                           // The final "End" also delivers the returned value.
+          },
+      });
+
+      // Exports enable features to be accessed by external code.
+      // Typically this means JavaScript, but this library adds .NET execution capability, too.
+      module.Exports.Add(new Export {
+        Kind = ExternalKind.Function,
+        Index = 0, // This should match the function index from above.
+        Name = "Demo", // Anything legal in Unicode is legal in an export name.
+      });
+
+      Debug.LogError("llama a!");
+
+      // We now have enough for a usable WASM file, which we could save with module.WriteToBinary().
+      // Below, we show how the Compile feature can be used for .NET-based execution.
+      // For stream-based compilation, WebAssembly.Compile should be used.
+      var instanceCreator = module.Compile<Sample>();
+
+      Debug.LogError("llama b!");
+
+      // Instances should be wrapped in a "using" block for automatic disposal.
+      // This sample doesn't import anything, so we pass an empty import dictionary.
+      using (var instance = instanceCreator(new ImportDictionary())) {
+        Debug.LogError("llama c!");
+
+        // FYI, instanceCreator can be used multiple times to create independent instances.
+        Debug.LogError(instance.Exports.Demo(0)); // Binary 0, result 0
+        Debug.LogError(instance.Exports.Demo(1)); // Binary 1, result 1,
+        Debug.LogError(instance.Exports.Demo(42));  // Binary 101010, result 3
+
+        Debug.LogError("llama d!");
+      } // Automatically release the WebAssembly instance here.
+
+      Debug.LogError("llama e!");
     }
 
     public float GetTime() {
